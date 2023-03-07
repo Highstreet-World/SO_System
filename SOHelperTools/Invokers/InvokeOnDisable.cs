e735a4370651dd2fa750f6cf5a8684e44f7510b4
posixCode <ovais221@gmail.com>
2023-03-06 22:52:28 -0500

    made invoke on disable script

---
+++ SOHelperTools/Invokers/InvokeOnDisable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class InvokeOnDisable : MonoBehaviour
+{
+    [FormerlySerializedAs("OnDisable")] public UnityEvent onDisable;
+    bool isFirstTime = true;
+    private void Start()
+    {
+        isFirstTime = false;
+        OnDisable();
+    }
+
+    private void OnDisable()
+    {
+        if (isFirstTime) return;
+        onDisable.Invoke();
+    }
+}
